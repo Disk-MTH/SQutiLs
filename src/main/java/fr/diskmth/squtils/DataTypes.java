dee60fae48fe8df97c3aeda18c4f5807b3432f62
Disk-MTH <gillet.mat@free.fr>
2022-08-05 12:24:59 +0200

    initital commit

---
+++ src/main/java/fr/diskmth/squtils/DataTypes.java
@@ -0,0 +1,10 @@
+package fr.diskmth.squtils;
+
+public class DataTypes
+{
+    public static final DataType UNKNOWN = new DataType("");
+    public static final DataType BOOLEAN = new DataType("boolean");
+    public static final DataType INTEGER = new DataType("int");
+    public static final DataType FLOAT = new DataType("float");
+    public static final DataType STRING = new DataType("varchar");
+}
